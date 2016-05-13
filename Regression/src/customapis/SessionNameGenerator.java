620c90c5b01771c3cd95e5144994f26217c49ac7
bjweber <brad.weber@autotrader.com>
2016-05-13 10:42:00 -0400

    changes

---
+++ Regression/src/customapis/SessionNameGenerator.java
@@ -0,0 +1,80 @@
+package customapis;
+
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+import com.provar.core.model.base.api.ValueScope;
+import com.provar.core.testapi.ITestExecutionContext;
+import com.provar.core.testapi.annotations.TestApi;
+import com.provar.core.testapi.annotations.TestApiExecutor;
+import com.provar.core.testapi.annotations.TestApiParameter;
+import com.provar.core.testapi.annotations.TestApiParameterGroup;
+import com.provar.core.testapi.annotations.TestApiParameterGroups;
+import com.provar.core.testapi.annotations.TestExecutionContext;
+import com.provar.core.testapi.annotations.TestLogger;
+
+@TestApi( title="Session Name Generator"
+        , summary=""
+        , remarks=""
+        , iconBase=""
+        , defaultApiGroups={"My Test APIs"}
+        )
+@TestApiParameterGroups(parameterGroups={
+	    @TestApiParameterGroup(groupName="inputs", title="Inputs"),
+	    @TestApiParameterGroup(groupName="result", title="Result"),
+	    })
+public class SessionNameGenerator {
+    
+
+    @TestApiParameter(seq=10, 
+            summary="The name that the result will be stored under.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="result")
+    public String resultName;
+
+    @TestApiParameter(seq=11, 
+            summary="The lifespan of the result.",
+            remarks="",
+            mandatory=true,
+            parameterGroup="result",
+            defaultValue="Test")
+    public ValueScope resultScope;
+
+    /** 
+     * Used to write to the test execution log.
+     */
+    @TestLogger
+    public Logger testLogger;
+    
+    /** 
+     * Provides access to facilities, mainly to set and get variable values.
+     */
+    @TestExecutionContext
+    public ITestExecutionContext testExecutionContext;
+    
+    @TestApiExecutor
+    public void execute() {
+
+    	Random random = new Random();
+    	String str = generateString(random, 
+    			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 20);
+    	
+    	
+    	String dummyResult = str;
+        testExecutionContext.setValue(resultName, dummyResult, resultScope);
+        
+    }
+    
+    public static String generateString(Random rng, String characters, int length)
+    {
+        char[] text = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            text[i] = characters.charAt(rng.nextInt(characters.length()));
+        }
+        return new String(text);
+    }
+    
+}
